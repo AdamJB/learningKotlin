7997491afb411de4761a1719da06d109e3364856
Adam Bednarek <adamjb51@gmail.com>
2018-03-29 12:57:12 -0400

    Add Teacher class

---
+++ src/Teacher.kt
@@ -0,0 +1,8 @@
+class Teacher(
+        val firstName: String,
+        val lastName: String,
+        val age: Int,
+        val gender: String,
+        val currentSubject: String,
+        val students: List<Student>) {
+}
