bafac365a27d0bf688993f85e5e25047e9d96bcd
Adam Bednarek <adamjb51@gmail.com>
2018-03-29 12:53:03 -0400

    Add Student class'

---
+++ src/Student.kt
@@ -0,0 +1,8 @@
+class Student(
+        val firstName: String,
+        val lastName: String,
+        val age: Int,
+        val gender: String,
+        val favoriteSubject: String,
+        val numericGrade: Int) {
+}
